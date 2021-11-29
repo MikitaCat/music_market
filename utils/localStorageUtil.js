cf7162814e8f2b16ece32fb3c494c574f9a6f19b
MikitaCat <nikita.proger21@gmail.com>
2021-11-29 16:05:17 +0300

    Implemented a class for working with local storage

---
+++ utils/localStorageUtil.js
@@ -0,0 +1,35 @@
+class LocalStorageUtil {
+  constructor() {
+    this.keyName = 'products';
+  }
+
+  getProducts() {
+    const productsLocalStorage = localStorage.getItem(this.keyName);
+    if (productsLocalStorage !== null) {
+      return JSON.parse(productsLocalStorage);
+    }
+    return [];
+  }
+
+  putProducts(id) {
+    let products = this.getProducts();
+    let isPushedProduct = false;
+
+    // Checking for matches
+    const index = products.indexOf(id);
+    if (index === -1) {
+      products.push(id);
+      isPushedProduct = true;
+    } else {
+      products.splice(index, 1);
+    }
+
+    //Set to the local storage
+    localStorage.setItem(this.keyName, JSON.stringify(products));
+
+    return {
+      isPushedProduct,
+      products,
+    };
+  }
+}
